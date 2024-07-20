e0b19eda60b9fffd4072ebe5f1060e6429307639
kidMax25 <snmaxis@gmail.com>
2024-07-20 18:19:28 +0300

    Landing Page

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter, Poppins } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+});
+
+export const metadata: Metadata = {
+  title: "Habitual",
+  description: "App for Men",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home(){
+  return <div>
+    <h1 className="poppins font-bold">Habitual</h1>
+  </div>
+}
